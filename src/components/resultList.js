005f89134defb4485c5b4c119553bc0a09b37b75
maximilian.ungar <maximilian.ungar@klarna.com>
2022-06-22 18:01:59 +0200

    refactor: use SingleResult component

--- src/components/resultList.js
+++ src/components/resultList.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { SingleResult } from './singleResult'
 
 const ResultList = ({ repositories, totalCount }) => {
   return (
@@ -6,20 +7,7 @@ const ResultList = ({ repositories, totalCount }) => {
       <h3>{'Current results'}</h3>
       <span>{`(${repositories?.length} / ${totalCount})`}</span>
       {repositories.map((repository) => (
-        <div
-          key={repository.full_name}
-          style={{
-            width: '50%',
-            minHeight: '3rem',
-            border: '1px solid green',
-            borderRadius: '5px',
-            margin: '5px',
-            paddingLeft: '10px'
-          }}>
-          {' '}
-          <p>{repository.full_name}</p>
-          <p>{repository.description}</p>
-        </div>
+        <SingleResult key={repository.full_name} name={repository.full_name} description={repository.description} />
       ))}
     </div>
   )
